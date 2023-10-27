fb57ed2b8256ddba4947fef6c21c393eba60a4a7
jefry pardo <jefry.pardo@tucompra.com.co>
2023-10-27 15:57:44 -0500

    plantilla basica

--- src/routes/mail/mail.route.ts
+++ src/routes/mail/mail.route.ts
@@ -3,13 +3,61 @@ import { transporter } from "../../service/sendmail.service";
 
 const routerMail = Router();
 
+const facturaHTML = `
+<!DOCTYPE html>
+<html>
+<head>
+    <title>Factura</title>
+</head>
+<body>
+    <h1>Factura</h1>
+    <table>
+        <thead>
+            <tr>
+                <th>Nombre del Producto</th>
+                <th>Cantidad</th>
+                <th>Precio Unitario</th>
+                <th>Precio Total</th>
+            </tr>
+        </thead>
+        <tbody>
+            <tr>
+                <td>Producto 1</td>
+                <td>2</td>
+                <td>$10.00</td>
+                <td>$20.00</td>
+            </tr>
+            <tr>
+                <td>Producto 2</td>
+                <td>3</td>
+                <td>$15.00</td>
+                <td>$45.00</td>
+            </tr>
+            <tr>
+                <td>Producto 3</td>
+                <td>1</td>
+                <td>$30.00</td>
+                <td>$30.00</td>
+            </tr>
+        </tbody>
+        <tfoot>
+            <tr>
+                <td colspan="3">Total:</td>
+                <td>$95.00</td>
+            </tr>
+        </tfoot>
+    </table>
+</body>
+</html>
+`;
+
 routerMail.get('/enviar-correo', (req, res) => {
     
     const mailOptions = {
       from: 'tucompraheroku@gmail.com',
       to: 'jeffryjhoan1996@gmail.com',
-      subject: 'Asunto del correo',
-      text: 'Contenido del correo',
+      subject: 'Cotización',
+      html: facturaHTML,
     };
   
     transporter.sendMail(mailOptions, (error:any, info:any) => {
