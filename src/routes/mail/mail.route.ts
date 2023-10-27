e971a76898bf0faed538e5a63ea0a99a83415dac
jefry pardo <jefry.pardo@tucompra.com.co>
2023-10-27 17:20:57 -0500

    se implementa plantilla dinamica

--- src/routes/mail/mail.route.ts
+++ src/routes/mail/mail.route.ts
@@ -1,63 +1,20 @@
 import { Router } from "express";
 import { transporter } from "../../service/sendmail.service";
+import { getPlantilla } from "./plantilla";
 
 const routerMail = Router();
 
-const facturaHTML = `
-<!DOCTYPE html>
-<html>
-<head>
-    <title>Factura</title>
-</head>
-<body>
-    <h1>Factura</h1>
-    <table>
-        <thead>
-            <tr>
-                <th>Nombre del Producto</th>
-                <th>Cantidad</th>
-                <th>Precio Unitario</th>
-                <th>Precio Total</th>
-            </tr>
-        </thead>
-        <tbody>
-            <tr>
-                <td>Producto 1</td>
-                <td>2</td>
-                <td>$10.00</td>
-                <td>$20.00</td>
-            </tr>
-            <tr>
-                <td>Producto 2</td>
-                <td>3</td>
-                <td>$15.00</td>
-                <td>$45.00</td>
-            </tr>
-            <tr>
-                <td>Producto 3</td>
-                <td>1</td>
-                <td>$30.00</td>
-                <td>$30.00</td>
-            </tr>
-        </tbody>
-        <tfoot>
-            <tr>
-                <td colspan="3">Total:</td>
-                <td>$95.00</td>
-            </tr>
-        </tfoot>
-    </table>
-</body>
-</html>
-`;
+
+
+
 
 routerMail.get('/enviar-correo', (req, res) => {
     
     const mailOptions = {
-      from: 'tucompraheroku@gmail.com',
-      to: 'jeffryjhoan1996@gmail.com',
+      from: 'pardo0alzate@gmail.com',
+      to: 'jeffry.pardo01@unicatolica.edu.co',
       subject: 'Cotización',
-      html: facturaHTML,
+      html: getPlantilla(),
     };
   
     transporter.sendMail(mailOptions, (error:any, info:any) => {
