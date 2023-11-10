dd8112899b6c6de72f1720fed24f9de7b9ffe97a
jefry pardo <jefry.pardo@tucompra.com.co>
2023-11-10 17:19:36 -0500

    insert sql producto

--- src/assets/query.create.bd.sql
+++ src/assets/query.create.bd.sql
@@ -101,13 +101,16 @@ CREATE TABLE Cotizacion (
     id_usuario VARCHAR(255)
 );
 
+-- productos repositorio img
+-- https://github.com/JefryPardo/tesis-img/tree/master/producto
+
 INSERT INTO usuario (id, nombre,apellido, direccion, celular,fecha_creacion, intentos_fallidos, clave, usuario, estado) VALUES ('a2fbca23-dbdb-42f5-beeb-448cf0a19e00','jeff','pardo','call 1','3022318153','2023-10-23 13:32:00','0','$2b$10$lB.eLeFr0xz2Xl7hQ32THu9k/rYW2P0WXKMWxCOrL.8fIQ34NyhDW','jeffryjhoan1996@gmail.com','activo');
 INSERT INTO rol (id,rol, estado) VALUES ('b19517e2-b383-4656-8099-67d49ca3a8c7','usuario','activo');
 INSERT INTO usuario_rol (id_usuario, id_rol) VALUES ('a2fbca23-dbdb-42f5-beeb-448cf0a19e00','b19517e2-b383-4656-8099-67d49ca3a8c7');
 
 INSERT INTO producto (nombre, descripcion, url_imagen, referencia, referencia_local, precio, ficha_tecnica, unidades, estado, categoria, tipo, marca)
 VALUES
-    ('Laptop Acer', 'Una poderosa laptop Acer', ARRAY['https://olimpica.vtexassets.com/arquivos/ids/1065780-1200-auto?v=638161519133700000&width=1200&height=auto&aspect=true'], '12345', 'ACER-001', 1230400.99, 'Especificaciones de la laptop', '10 unidades', 'activo', '', 'equipo', ''),
+    ('Cable para señal UTP Gris', 'Nuevo CABLE especial para SEÑAL DE VIDEO CCTV,  TIPO UTP CCA, pares trenzados Cat. 5E 4X2X24 AWG, diámetro 0,51 (Gris) PARA INTERIOR, ideal para distancias inferiores a 30 metros', ARRAY['https://raw.githubusercontent.com/JefryPardo/tesis-img/master/producto/CSIUTP5ECAG-CCTV.jpg'], 'CSIUTP5ECAG-CCTV', 'CABLES SUPERIORES', 1300.99, '', '1582', 'activo', '', 'cable', ''),
     ('Teléfono Samsung Galaxy S21', 'Un teléfono inteligente de alta gama', ARRAY['https://www.alkosto.com/medias/750Wx750H-master-hotfolder-transfer-incoming-deposit-hybris-interfaces-IN-media-product-8806092837812-001.jpg?context=bWFzdGVyfGltYWdlc3wyMDYzODZ8aW1hZ2UvanBlZ3xhRGsxTDJnNVlTOHhNemc1T1RNMU1EVXdOelUxTUM4M05UQlhlRGMxTUVoZmJXRnpkR1Z5TDJodmRHWnZiR1JsY2k5MGNtRnVjMlpsY2k5cGJtTnZiV2x1Wnk5a1pYQnZjMmwwTDJoNVluSnBjeTFwYm5SbGNtWmhZMlZ6TDBsT0wyMWxaR2xoTDNCeWIyUjFZM1F2T0Rnd05qQTVNamd6TnpneE1sOHdNREV1YW5CbnwyN2FkNjJmN2RjZTA1MDkxMmQ0NWM5NDM1YjdiZGFkNjVjOGI4ZGRhMjhiZDkyMWY2ZjU4ODhhMjg0MmEzNWVi'], '67890', 'SAMSUNG-001', 850799.00, 'Especificaciones del teléfono', '5 unidades', 'activo', '', 'equipo', ''),
     ('TV LED LG 55 pulgadas', 'Televisor LG con resolución 4K', ARRAY['https://www.alkosto.com/medias/750Wx750H-master-hotfolder-transfer-incoming-deposit-hybris-interfaces-IN-media-product-8806091856739-002.jpg?context=bWFzdGVyfGltYWdlc3wxNjg0Mjh8aW1hZ2UvanBlZ3xhRFV6TDJobFlTOHhNemd4TkRrek5UQTJNRFV4TUM4M05UQlhlRGMxTUVoZmJXRnpkR1Z5TDJodmRHWnZiR1JsY2k5MGNtRnVjMlpsY2k5cGJtTnZiV2x1Wnk5a1pYQnZjMmwwTDJoNVluSnBjeTFwYm5SbGNtWmhZMlZ6TDBsT0wyMWxaR2xoTDNCeWIyUjFZM1F2T0Rnd05qQTVNVGcxTmpjek9WOHdNREl1YW5CbnwzOWEwMTdlZmZkYWE3MjI3MjczOGNlNDIxNmNlODlkNWY4NzllOWQ3MDUwZmVjMjI1ZDlkNThmNDI1ZjRiNTMy'], '54321', 'LG-001', 958599.99, 'Especificaciones del televisor', '8 unidades', 'activo', '', 'material', ''),
     ('Cámara Canon EOS Rebel', 'Una cámara réflex digital', ARRAY['https://m.media-amazon.com/images/I/71EWRyqzw0L.jpg','https://m.media-amazon.com/images/I/717xSjyDRzL.jpg','https://m.media-amazon.com/images/I/71YqHRYSluL.jpg'], '98765', 'CANON-001', 120699.95, 'Especificaciones de la cámara', '12 unidades', 'activo', '', 'material', '');
